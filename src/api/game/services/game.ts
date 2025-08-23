dcf9677a37f3e32f358d1c7f89821dc1fae1015e
Felipe DR <felipe.corp7@gmail.com>
2025-08-23 14:57:47 -0300

    feat: class-06 creating many to many relational data

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -7,6 +7,12 @@ import axios from 'axios';
 import { JSDOM } from 'jsdom';
 import slugify from 'slugify';
 
+const gameService = 'api::game.game';
+const publisherService = 'api::publisher.publisher';
+const developerService = 'api::developer.developer';
+const categoryService = 'api::category.category';
+const platformService = 'api::platform.platform';
+
 async function getGameInfo(slug) {
   const gogSlug = slug.replaceAll('-', '_').toLowerCase();
 
@@ -54,6 +60,43 @@ async function create(name, entityService) {
   }
 }
 
+async function createManyToManyData(products) {
+  const developersSet = new Set();
+  const publishersSet = new Set();
+  const categoriesSet = new Set();
+  const platformsSet = new Set();
+
+  products.forEach((product) => {
+    const { developers, publishers, genres, operatingSystems } = product;
+
+    genres?.forEach(({ name }) => {
+      categoriesSet.add(name);
+    });
+
+    operatingSystems?.forEach((item) => {
+      platformsSet.add(item);
+    });
+
+    developers?.forEach((item) => {
+      developersSet.add(item);
+    });
+
+    publishers?.forEach((item) => {
+      publishersSet.add(item);
+    });
+  });
+
+  const createCall = (set, entityName) =>
+    Array.from(set).map((name) => create(name, entityName));
+
+  return Promise.all([
+    ...createCall(developersSet, developerService),
+    ...createCall(publishersSet, publisherService),
+    ...createCall(categoriesSet, categoryService),
+    ...createCall(platformsSet, platformService),
+  ]);
+}
+
 export default factories.createCoreService('api::game.game', () => ({
   async populate(params) {
     const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`;
@@ -62,16 +105,6 @@ export default factories.createCoreService('api::game.game', () => ({
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    products[0].developers.map(async (developer) => {
-      await create(developer, 'api::developer.developer');
-    });
-
-    products[0].publishers.map(async (publisher) => {
-      await create(publisher, 'api::publisher.publisher');
-    });
-
-    products[0].genres.map(async ({ name }) => {
-      await create(name, 'api::category.category');
-    });
+    await createManyToManyData([products[0], products[2]]);
   },
 }));
