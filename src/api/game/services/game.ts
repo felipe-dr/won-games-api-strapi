011a3e800807b9c8f32a4560e57963ff6a13cf63
Felipe DR <felipe.corp7@gmail.com>
2025-08-23 15:07:49 -0300

    feat: class-06 creating first game structure

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -97,7 +97,52 @@ async function createManyToManyData(products) {
   ]);
 }
 
-export default factories.createCoreService('api::game.game', () => ({
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, gameService);
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.service(`${gameService}`).create({
+          data: {
+            name: product.title,
+            slug: product.slug,
+            price: product.price.finalMoney.amount,
+            release_date: new Date(product.releaseDate),
+            categories: await Promise.all(
+              product.genres.map(({ name }) => getByName(name, categoryService))
+            ),
+            platforms: await Promise.all(
+              product.operatingSystems.map((name) =>
+                getByName(name, platformService)
+              )
+            ),
+            developers: await Promise.all(
+              product.developers.map((name) =>
+                getByName(name, developerService)
+              )
+            ),
+            publisher: await Promise.all(
+              product.publishers.map((name) =>
+                getByName(name, publisherService)
+              )
+            ),
+            ...(await getGameInfo(product.slug)),
+            publishedAt: new Date(),
+          },
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
+
+
+export default factories.createCoreService(gameService, () => ({
   async populate(params) {
     const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending`;
 
@@ -106,5 +151,6 @@ export default factories.createCoreService('api::game.game', () => ({
     } = await axios.get(gogApiUrl);
 
     await createManyToManyData([products[0], products[2]]);
+    await createGames([products[0], products[2]]);
   },
 }));
